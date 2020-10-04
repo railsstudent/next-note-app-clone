b4256ad6e051e25dcd95833a75d4e473f4028a99
Connie Leung <cleung334@gmail.com>
2020-10-04 20:20:02 +0800

    initial commit
    Signed-off-by: Connie Leung <cleung334@gmail.com>

---
+++ pages/notes/[id].tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+export default () => {
+    const router = useRouter();
+    const { id } = router.query;
+
+    return <h1>Note: {id}</h1>
+};
---
+++ pages/notes/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Notes = () => <h1>Notes Page</h1>
+
+export default Notes;
+    
